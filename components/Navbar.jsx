ba763e9d25322d52343940b23aa964f90dd239cf
sreekanth-glitch <princepardhu28@gmail.com>
2026-09-08 12:11:33 +0530

    Nav logo update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -19,7 +19,7 @@ export default function Navbar() {
     <header className="navbar">
       <div className="container nav-inner">
         <Link href="/" className="brand" onClick={() => setOpen(false)}>
-          <img src="/logo.jpg" alt="Sri Suryadeva Nursery logo" />
+          <img src="/logoo.jpg" alt="Sri Suryadeva Nursery logo" />
           <span>
             <strong>Sri Suryadeva</strong>
             <small>Nursery · Kadiyam</small>
